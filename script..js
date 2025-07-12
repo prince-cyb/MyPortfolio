30878845c9ba5abf4baa1a51161e31263c928494
Prince Chaurasiya <lovehint05@gmail.com>
2025-07-12 15:23:49 +0530

    Add files via upload

---
+++ script..js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const menu = document.querySelector(".menu");
+
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("show");
+  });
+
+  // Optional: close menu when clicking a link
+  document.querySelectorAll('.menu a').forEach(link => {
+    link.addEventListener('click', () => {
+      menu.classList.remove('show');
+    });
+  });
+});
+
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll("div[id]");
+  const navLinks = document.querySelectorAll(".menu a");
+
+  let current = "";
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 103;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
